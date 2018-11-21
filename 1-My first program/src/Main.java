1a69f265fb8d330049cb0c6adc47dbc341765285
admin <admin@192.168.13.220>
2018-11-21 13:37:30 -0500

    last version

--- 1-My first program/src/Main.java
+++ 1-My first program/src/Main.java
@@ -7,7 +7,7 @@ System.out.println("I love Java Script");
 System.out.print("Welcome to JAVA world");
 System.out.println("My name is Mike Smith");
 System.out.println("How are you");
-System.out.println("Good");
+System.out.println("Good bye");
 
 	}
 
